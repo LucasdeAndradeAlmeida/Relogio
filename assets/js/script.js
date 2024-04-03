b1a03c2a40fb056f47c2c4507b0854ec704c0677
Lucas Andrade <contatolucasdeandradealmeida@gmail.com>
2024-04-03 15:11:48 -0300

    primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const relogio = setInterval(function time() {
+
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let seg = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min;
+
+    if (seg < 10) seg = '0' + seg;
+
+    document.querySelector(".horas").textContent = hr;
+    document.querySelector(".minutos").textContent = min;
+    document.querySelector(".segundos").textContent = seg;
+
+    
+})
+
